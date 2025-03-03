a559fd9dd1a33014ab2fc77427c13d6e20fdd859
hashemUOP <hashemsughaier@gmail.com>
2025-03-04 01:24:14 +0300

    final version of homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "../src/styles/global.css"; // global css to all components
+
+
 const root = ReactDOM.createRoot(
   document.getElementById("root")
 );
