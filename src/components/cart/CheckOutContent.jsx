5c9bf9d5e12fb9bddbbb08769ec3b292daeb9696
hashemUOP <hashemsughaier@gmail.com>
2025-03-07 17:33:38 +0300

    checkout final version

--- src/components/cart/CheckOutContent.jsx
+++ src/components/cart/CheckOutContent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../../styles/checkout.module.css";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
-
+import img from "../../assets/img/81dfKK-q0AL.jpg";
 
 export default function CheckOutContent(){
     return (
@@ -12,7 +12,7 @@ export default function CheckOutContent(){
                         <div className={styles.row1L} style={{marginTop:"20px"}}>
                             <span>Select shipping country</span>
                         </div>
-                        <DropDownMenu/>
+                        <DropDownMenu text={"Select country"}/>
                         <hr style={{marginLeft:"4%",marginRight:"5%"}}/>
                         <div className={styles.row2L}>
                             <span>Shipping address</span>
@@ -23,7 +23,7 @@ export default function CheckOutContent(){
                         <form style={{width:"95%",marginLeft:"15px"}}>
                             <input type="text" className={styles.fullNameForm} placeholder="Enter your full name"></input>
                         </form>
-                        <div style={{display:"flex"}}>
+                        <div style={{display:"flex",}}>
                             <div style={{display:"flex",flexDirection:"column",width:"50%",marginLeft:"1%"}}>
                                 <div className={styles.row1L} style={{marginTop:"1px"}}>
                                     <span>Email address*</span>
@@ -41,11 +41,67 @@ export default function CheckOutContent(){
                                 </form>
                             </div>
                         </div>
-                        <CustomForm text={"Phone number*"} textHolder={"Enter your phone number"}/>
-                        <CustomForm text={"Full house address*"} textHolder={"Enter your full home address"}/>
+                        <CustomForm text={"Phone number*"} textHolder={"Enter your phone number"} marginT={"10px"}/>
+                        <CustomForm text={"Full home address*"} textHolder={"Enter your full home address"}/>
+                        <div style={{display:"flex",alignItems:"center",width:"100%"}}>
+                            <div style={{width:"50%",marginLeft:"8px"}}>
+                                <CustomForm text={"City*"} textHolder={"City"}/>   
+                            </div>
+                            <div style={{width:"50%",marginRight:"15px"}}>
+                                <DropDownMenu text={"Select Region"}/>    
+                            </div>
+                        </div>
+                        <CustomForm text={"Postal code*"} textHolder={"Enter your postal code"}/>
                     </div>
                     <div className={styles.contentRight}>
-
+                        <span style={{marginLeft:"20px",fontWeight:"bold",marginTop:"20px",marginBottom:"20px"}}>Your order</span>
+                        <div style={{display:"flex",marginLeft:"20px",justifyContent:"space-between",width:"90%"}}>
+                            <div style={{display:"flex"}}>
+                                <div className={styles.orderImgContainer}>
+                                    <img src={img} style={{width:"100%",height:"100%"}} />
+                                </div>
+                                <div style={{display:"flex",flexDirection:"column",justifyContent:"space-between",alignItems:"flex-start",marginLeft:"10px"}}>
+                                    <span style={{fontWeight:"bold"}}>Cashier Leather...</span>
+                                    <span style={{color:"grey"}}>Grey</span>
+                                    <span style={{fontWeight:"bold"}}>x1</span>
+                                </div>
+                            </div>
+                            <span style={{fontWeight:"bold",justifySelf:"flex-end",}}>$2500.00</span>
+                        </div>   
+                        <div style={{ width: "91%",marginLeft:"20px"}}>
+                            <hr />
+                        </div>
+                        <div style={{display:"flex",flexDirection:"column",justifyContent:"flex-start",alignItems:"flex-start",width:"100%"}}>
+                            <span style={{fontWeight:"bold",marginLeft:"22px",marginTop:"5px"}}>Discount Code</span>
+                            <div style={{display:"flex",width:"100%",alignItems:"center"}}>
+                                <div style={{width:"80%"}}>
+                                    <CustomForm textHolder={"Add discount code"}/>   
+                                </div>
+                                <div style={{width:"20%",marginRight:"15px"}}>
+                                    <button className={styles.orderButton}>Apply</button>
+                                </div>
+                            </div>
+                        </div> 
+                        <div style={{display:"flex",width:"100%"}}>
+                            <span style={{fontWeight:"bold",fontSize:"13px",marginLeft:"22px"}}>New customer?</span>
+                            <span style={{textDecoration:"underline",fontWeight:"300",fontSize:"13px",marginLeft:"5px",cursor:"pointer"}}>Sign up</span>
+                            <span style={{fontWeight:"300",fontSize:"13px",marginLeft:"5px"}}>to get better offers</span>
+                        </div>  
+                        <div style={{ width: "91%",marginLeft:"20px"}}>
+                            <hr />
+                        </div>     
+                        <SeperateText textLeft={"Subtotal"} textRight={"$2500.00"} isOne/>
+                        <SeperateText textLeft={"Discount"} textRight={"-$0.00"} />
+                        <SeperateText textLeft={"Shipment cost"} textRight={"$22.50"} />
+                        <div style={{ width: "91%",marginLeft:"20px"}}>
+                            <hr />
+                        </div>  
+                        <SeperateText textLeft={"Grandtotal"} textRight={"$2522.50"} isOne isLast/>
+                        <div style={{width:"100%",marginTop:"20px"}}>
+                            <button style={{backgroundColor:"black",color:"white",width:"90%",height:"50px",borderRadius:"10px"}}>
+                                Continue to payment
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -53,12 +109,12 @@ export default function CheckOutContent(){
     );
 }
 
-function DropDownMenu(){
+function DropDownMenu({text}){
     return (
             <div class="dropdown">
             <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false" className={styles.customButton}>
                 <div style={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
-                    <span>Select country</span>
+                    <span>{text}</span>
                     <MdOutlineKeyboardArrowDown size={24} />
                 </div>
             </button>
@@ -71,15 +127,24 @@ function DropDownMenu(){
     );
 }
 
-function CustomForm({text,textHolder}){
+function CustomForm({text,textHolder,marginT}){
     return(
-        <>
-        <div className={styles.row1L} style={{marginTop:"10px"}}>
-            <span>{text}</span>
+        <div style={{display:"flex",flexDirection:"column"}}>
+            <div className={styles.row1L} style={{marginTop:marginT}}>
+                <span>{text}</span>
+            </div>
+            <form style={{width:"95%",marginLeft:"15px",marginBottom:"15px"}}>
+                <input type="text" className={styles.fullNameForm} placeholder={textHolder}></input>
+            </form>   
         </div>
-        <form style={{width:"95%",marginLeft:"15px"}}>
-            <input type="text" className={styles.fullNameForm} placeholder={textHolder}></input>
-        </form>
-        </>
+        
+    );
+}
+function SeperateText({textRight,textLeft,isOne,isLast}){
+    return (
+        <div style={{display:"flex",width:"100%",justifyContent:"space-between",alignItems:"center"}}>
+            <span style={{color:isLast? "black":"gray",fontWeight:isLast?"bold":"500",marginLeft:"22px"}}>{textLeft}</span>
+            <span style={{color:isOne? "black" : "grey",fontWeight:"600",marginRight:"22px"}}>{textRight}</span>
+        </div>
     );
 }
