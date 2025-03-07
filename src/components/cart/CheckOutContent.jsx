8ddf9c95fa92a664a33d806cdec72a4724f35534
hashemUOP <hashemsughaier@gmail.com>
2025-03-07 03:12:16 +0300

    update checkout

--- src/components/cart/CheckOutContent.jsx
+++ src/components/cart/CheckOutContent.jsx
@@ -9,10 +9,40 @@ export default function CheckOutContent(){
             <div className={styles.gridItemContent}>
                 <div className={styles.contentContainer}>
                     <div className={styles.contentLeft}>
-                        <div className={styles.row1L}>
+                        <div className={styles.row1L} style={{marginTop:"20px"}}>
                             <span>Select shipping country</span>
                         </div>
                         <DropDownMenu/>
+                        <hr style={{marginLeft:"4%",marginRight:"5%"}}/>
+                        <div className={styles.row2L}>
+                            <span>Shipping address</span>
+                        </div>
+                        <div className={styles.row1L} style={{marginTop:"10px"}}>
+                            <span>Full name*</span>
+                        </div>
+                        <form style={{width:"95%",marginLeft:"15px"}}>
+                            <input type="text" className={styles.fullNameForm} placeholder="Enter your full name"></input>
+                        </form>
+                        <div style={{display:"flex"}}>
+                            <div style={{display:"flex",flexDirection:"column",width:"50%",marginLeft:"1%"}}>
+                                <div className={styles.row1L} style={{marginTop:"1px"}}>
+                                    <span>Email address*</span>
+                                </div>
+                                <form style={{width:"95%",marginLeft:"15px"}}>
+                                    <input type="text" className={styles.fullNameForm} placeholder="Enter your email address"></input>
+                                </form>
+                            </div>
+                            <div style={{display:"flex",flexDirection:"column",width:"50%",marginRight:"4%"}}>
+                                <div className={styles.row1L} style={{marginTop:"1px"}}>
+                                    <span>Confirmation email*</span>
+                                </div>
+                                <form style={{width:"95%",marginLeft:"15px"}}>
+                                    <input type="text" className={styles.fullNameForm} placeholder="Enter your email confirmation"></input>
+                                </form>
+                            </div>
+                        </div>
+                        <CustomForm text={"Phone number*"} textHolder={"Enter your phone number"}/>
+                        <CustomForm text={"Full house address*"} textHolder={"Enter your full home address"}/>
                     </div>
                     <div className={styles.contentRight}>
 
@@ -38,5 +68,18 @@ function DropDownMenu(){
                 <li><a class="dropdown-item" href="#">Action 3</a></li>
             </ul>
             </div>
+    );
+}
+
+function CustomForm({text,textHolder}){
+    return(
+        <>
+        <div className={styles.row1L} style={{marginTop:"10px"}}>
+            <span>{text}</span>
+        </div>
+        <form style={{width:"95%",marginLeft:"15px"}}>
+            <input type="text" className={styles.fullNameForm} placeholder={textHolder}></input>
+        </form>
+        </>
     );
 }
