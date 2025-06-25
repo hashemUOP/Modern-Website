93a3c53452288678c210d79977d5472f96d651c5
hashemUOP <hashemsughaier@gmail.com>
2025-06-26 02:06:02 +0300

    contact page final update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,8 @@ import styles from "../styles/about-us.module.css";
 import { Header } from "../components/index/Header";
 import Footer from "../components/index/Footer";
 import { FaArrowRight } from "react-icons/fa6";
+import img from "../assets/img/call-center-agents-avatars-collection-600nw-2303842997-removebg-preview.png";
+
 
 function Contact(){
   return (
@@ -23,10 +25,10 @@ function Contact(){
                                 <p style={{fontSize:"90%"}}>Our friendly team would love to hear from you</p>
                                 <form className={styles.form} action="/submit" method="POST">
                                     <label for="name">Name</label>
-                                    <input type="text" id="name" name="name" required />
+                                    <input type="text" id="name" placeholder="name" name="name" required />
 
                                     <label for="email">Email*</label>
-                                    <input type="email" id="email" name="email" required />
+                                    <input type="email" id="email" placeholder="email" name="email" required />
 
                                     <label for="message">Your request</label>
                                     <textarea id="message" name="message" class="textArea" placeholder="Your request..." required></textarea>
@@ -36,7 +38,20 @@ function Contact(){
                             </div>
                         </div>
                         <div className={styles.gridBox2}>
-                            <p>Alsughaier</p>
+                            <div className={styles.box2Content}>
+                                <img src={img} style={{width:"50%",marginTop:"120px"}} alt="call center avatar"></img>
+                                <p style={{fontWeight:"bold",marginTop:"20px"}}>Need Further Assisstance?</p>
+                                <p style={{fontSize:"13px",marginLeft:"100px",marginRight:"100px"}}>Our customer support is available Monday to Friday: 11am-7pm</p>
+                                <p style={{fontSize:"13px",marginLeft:"100px",marginRight:"100px"}}>We can help answer any questions about our products, shipping or anything else on your mind!</p>
+                                <p>
+                                    <span style={{fontWeight:"bolder"}}>Call:</span>
+                                    <span style={{color:"#F8F8F8",fontSize:"13px"}}> (123)-456-7890</span>
+                                </p>
+                                <p>
+                                    <span style={{fontWeight:"bolder"}}>Visit:</span>
+                                    <span style={{color:"#F8F8F8",fontSize:"13px"}}> 1234 Example Street, Anytown, ST 12345                                    </span>
+                                </p>
+                            </div>
                         </div>
                 </div>              
           </div>
